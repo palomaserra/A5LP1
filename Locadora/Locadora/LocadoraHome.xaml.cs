a9325becb9e9a379d3b4eb1c7a02c1cfb5ab12c8
DESKTOP-N9ET2EI\Samara Rocha Lipolis <samararochalipolis@gmail.com>
2018-05-25 20:59:51 -0300

    Estrutura inicial do projeto

---
+++ Locadora/Locadora/LocadoraHome.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Locadora
+{
+    /// <summary>
+    /// Interação lógica para LocadoraHome.xam
+    /// </summary>
+    public partial class LocadoraHome : Page
+    {
+        public LocadoraHome()
+        {
+            InitializeComponent();
+        }
+
+        private void selecionarFilme(object sender, RoutedEventArgs e)
+        {
+            // Ver filme
+            LocadoraFilme filme = new LocadoraFilme(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(filme);
+        }
+    }
+}
